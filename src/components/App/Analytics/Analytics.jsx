8c1519ae0a0f069984a646a6ca51ef0791e14302
James Armstead <armsteadj1@gmail.com>
2017-09-02 07:30:34 -0500

    adding path not url

--- src/components/App/Analytics/Analytics.jsx
+++ src/components/App/Analytics/Analytics.jsx
@@ -3,6 +3,6 @@ import '../../RacerIcon/RacerIcon.css';
 import ua from 'universal-analytics';
 
 export const Analytics = () => {
-  ua("UA-105800781-1").pageview(window.location.href).send();
+  ua("UA-105800781-1").pageview(window.location.pathname).send();
   return (<span />);
 };
