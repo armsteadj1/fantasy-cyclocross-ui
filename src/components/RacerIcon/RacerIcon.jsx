c21a96cdfea9eaaa0d5040c374c001853fb25365
James Armstead <armsteadj1@gmail.com>
2017-09-07 14:52:00 +0000

    us champs!

--- src/components/RacerIcon/RacerIcon.jsx
+++ src/components/RacerIcon/RacerIcon.jsx
@@ -4,6 +4,8 @@ import './RacerIcon.css';
 const images = {
   'Joe PetersenDes Moines, IA': 'https://bargeek.files.wordpress.com/2009/03/pbr_can.jpg',
   'Chad DonahueDes Moines, IA': 'http://image.redbull.com/rbx00264/0100/0/406/products/packshots/en_GB/Red-Bull-Energy-Drink-Can-UK.png',
+  'Stephen HydeEasthampton, MA': 'https://images.vexels.com/media/users/3/128974/isolated/preview/2109e6c1876ec147437565f9dceec14c-usa-flag-flying-eagle-by-vexels.png',
+  'Katherine ComptonColorado Springs, CO': 'https://images.vexels.com/media/users/3/128974/isolated/preview/2109e6c1876ec147437565f9dceec14c-usa-flag-flying-eagle-by-vexels.png',
 };
 
 export const RacerIcon = ({name, location}) => (
