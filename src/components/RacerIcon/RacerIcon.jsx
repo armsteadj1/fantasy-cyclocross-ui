7cd2ca7bb1321c9efb00954c8f83c4e9566f2149
James Armstead <armsteadj1@gmail.com>
2017-09-07 14:59:58 +0000

    new image.

--- src/components/RacerIcon/RacerIcon.jsx
+++ src/components/RacerIcon/RacerIcon.jsx
@@ -4,8 +4,8 @@ import './RacerIcon.css';
 const images = {
   'Joe PetersenDes Moines, IA': 'https://bargeek.files.wordpress.com/2009/03/pbr_can.jpg',
   'Chad DonahueDes Moines, IA': 'http://image.redbull.com/rbx00264/0100/0/406/products/packshots/en_GB/Red-Bull-Energy-Drink-Can-UK.png',
-  'Stephen HydeEasthampton, MA': 'https://images.vexels.com/media/users/3/128974/isolated/preview/2109e6c1876ec147437565f9dceec14c-usa-flag-flying-eagle-by-vexels.png',
-  'Katherine ComptonColorado Springs, CO': 'https://images.vexels.com/media/users/3/128974/isolated/preview/2109e6c1876ec147437565f9dceec14c-usa-flag-flying-eagle-by-vexels.png',
+  'Stephen HydeEasthampton, MA': 'https://cdn.pixabay.com/photo/2016/04/06/13/18/american-flag-1311744_960_720.png',
+  'Katherine ComptonColorado Springs, CO': 'https://cdn.pixabay.com/photo/2016/04/06/13/18/american-flag-1311744_960_720.png',
 };
 
 export const RacerIcon = ({name, location}) => (
