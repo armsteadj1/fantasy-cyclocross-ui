9fc70507471ee25057aaa9eb1bdf45e74cdcf370
James Armstead <armsteadj1@gmail.com>
2017-09-07 07:53:00 -0500

    making analytics not look like its for only one person

--- src/components/App/Analytics/Analytics.jsx
+++ src/components/App/Analytics/Analytics.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../../RacerIcon/RacerIcon.css';
 import ua from 'universal-analytics';
+import uuid from 'uuid/v4';
+import Cookie from 'js-cookie';
 
 export const Analytics = () => {
-  ua("UA-105800781-1").pageview(window.location.pathname).send();
+  const id = Cookie.get('analytics_id');
+  if(!id) {
+    Cookie.set('analytics_id', uuid());
+  }
+
+  ua("UA-105800781-1", Cookie.get('analytics_id')).pageview(window.location.pathname).send();
   return (<span />);
 };
