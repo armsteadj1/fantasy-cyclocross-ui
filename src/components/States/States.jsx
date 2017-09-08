05238872f8d157d3fcd0a1c03f8f6c6922af96d3
James Armstead <armsteadj1@gmail.com>
2017-09-08 07:49:33 -0500

    geo

--- src/components/States/States.jsx
+++ src/components/States/States.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
+import { states } from '../../helpers/USStates.service';
+import { distance, getLocation } from './GeoIp.service';
 import { StateButton } from './StateButton/StateButton';
 import './States.css';
-import { states } from '../../helpers/USStates.service';
 
 export class States extends React.Component {
   constructor(state) {
     super(state);
+    this.state = {
+      states: this.getStateGroups(states.slice(0))
+    };
+  }
+
+  getStateGroups = states => {
     const statesArray = [];
-    while(states.length) {
+    while (states.length) {
       statesArray.push(states.splice(0, 4));
     }
-    this.state = {
-      states: statesArray
-    };
+    return statesArray;
+  };
+
+  componentDidMount() {
+    getLocation().then(location => {
+      const s = states.map(state => {
+        state.distance = distance(state, location);
+        return state;
+      });
+
+      this.setState({
+        states: this.getStateGroups(s.slice(0).sort((a,b) => a.distance - b.distance))
+      })
+    });
   }
 
   render() {
@@ -26,16 +44,17 @@ export class States extends React.Component {
                     <br />
                   </Col>
                 </Row>
-               {
-                this.state.states.map(stateGroup =>
-                  <Row className="state-row" >
-                    {stateGroup.map(state =>
-                      <Col md={3} sm={6} xs={12} >
-                        <StateButton name={state.name} abbreviation={state.abbreviation} />
-                      </Col>
-                    )}
-                  </Row>)
-               }
+        {
+          this.state.states.map(stateGroup =>
+            <Row className="state-row" >
+              {stateGroup.map(state =>
+                <Col md={3} sm={6} xs={12} >
+                  <StateButton name={state.name} abbreviation={state.abbreviation} />
+                </Col>
+              )}
+            </Row>)
+        }
                </span>
-    )};
+    )
+  };
 }
